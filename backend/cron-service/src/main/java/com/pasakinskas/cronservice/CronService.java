4c25a68fd113f652c0f515f68a57ffedc76a6245
Marius <marius@Mariuss-MBP.lan>
2020-06-14 23:28:18 +0300

    add scheduler cron service

---
+++ backend/cron-service/src/main/java/com/pasakinskas/cronservice/CronService.java
@@ -0,0 +1,28 @@
+package com.pasakinskas.cronservice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import javax.xml.ws.WebServiceException;
+
+@Component
+public class CronService {
+
+    @Value("${service.api-url}")
+    private String API_URL;
+
+    @Scheduled(fixedDelayString = "${service.repeat-interval}")
+    public void hitApi() {
+        ResponseEntity<String> response = new RestTemplate()
+                .getForEntity(API_URL, String.class);
+
+        if (response.getStatusCode().isError()) {
+            throw new WebServiceException("Error contacting currency converter server");
+        }
+        System.out.println(response.getStatusCode());
+    }
+}
